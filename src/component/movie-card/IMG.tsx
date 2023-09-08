046d9eef9ed9824c16240d390d6caac4fc6d0ccf
kamalov-eldar <kamalov-eldar@mail.ru>
2023-09-08 23:38:39 +0300

    fix img hero slide item

--- src/component/movie-card/IMG.tsx
+++ src/component/movie-card/IMG.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 type IMGProps = {
     path: string | null;
-    size: string;
+    size?: string;
     mods?: string;
     link?: string;
 };
@@ -14,7 +14,7 @@ export const IMG: FC<IMGProps> = ({ size, path, mods, link }) => {
     const [url, setUrl] = useState("");
 
     useEffect(() => {
-        fetch(`https://image.tmdb.org/t/p/w220_and_h330_face/${path}`)
+        fetch(`https://image.tmdb.org/t/p/${size ? size : "w220_and_h330_face"}/${path}`)
             .then((response) => {
                 return response.blob();
             })
@@ -28,17 +28,26 @@ export const IMG: FC<IMGProps> = ({ size, path, mods, link }) => {
             .catch((err) => console.log(err));
     }, []);
 
-    if (!url) {
-        return <Skeleton border="10px"  borderBottomRightRadius={"0"} borderBottomLeftRadius={"0"}  paddingTop="150.5%" />;
+    if (mods && !url) {
+        return <></>;
     }
 
+    if (mods && url) {
+        return <img src={url} className="card__img" />;
+    }
+
+    if (!url && !mods) {
+        return <Skeleton border="10px" borderBottomRightRadius={"0"} borderBottomLeftRadius={"0"} paddingTop="150.5%" />;
+    }
 
     return (
         <Link to={link || "/"} className="card__poster">
+            {/*  <div className="card__img-block"> */}
             <img src={url} className="card__img" />
             <Button className="btn">
                 <i className="bx bx-play"></i>
             </Button>
+            {/*  </div> */}
         </Link>
     );
 };
