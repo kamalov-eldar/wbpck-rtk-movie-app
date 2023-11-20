58c700f6595b8ad3b08c66a49909299e5e244b0a
kamalov-eldar <kamalov-eldar@mail.ru>
2023-11-20 15:25:36 +0300

    add authAxios.interceptors

--- src/api/authClient.ts
+++ src/api/authClient.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
-import apiConfig from "./apiConfig";
-import queryString from "query-string";
 
 const authAxios = axios.create({
     baseURL: __API__, //  "http://localhost:8001",
     headers: {
         authorization: localStorage.getItem("user") || "",
     },
+});
+
+authAxios.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem("user") || "";
+    }
+    return config;
 });
 
 export default authAxios;
