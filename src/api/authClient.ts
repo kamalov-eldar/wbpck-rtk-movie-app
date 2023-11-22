d43461ac359b841d169de4f3489942abf818fcb4
kamalov-eldar <kamalov-eldar@mail.ru>
2023-11-22 17:45:43 +0300

    "Content-Type": "application/json",

--- src/api/authClient.ts
+++ src/api/authClient.ts
@@ -4,6 +4,7 @@ const authAxios = axios.create({
     baseURL: __API__, //  "http://localhost:8001",
     headers: {
         authorization: localStorage.getItem("user") || "",
+        "Content-Type": "application/json",
     },
 });
 
